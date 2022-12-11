ed103263f52c54532a05d0b2525f3cc8e20ca1f0
EasyDove <116106656+EasyDove@users.noreply.github.com>
2022-12-11 14:24:40 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,101 @@
+var goback = document.getElementById("goback");
+
+document.getElementById("btnLv1").onclick = function () {
+  document.getElementById("btnLv1Anim").style.top = "200px";
+  document.getElementById("btnLv1Anim").style.visibility = "hidden";
+  document.getElementById("btnLv1Anim").style.opacity = "0";
+
+  document.getElementById("btn2Lv1Anim").style.top = "0px";
+  document.getElementById("btn2Lv1Anim").style.visibility = "visible";
+  document.getElementById("btn2Lv1Anim").style.opacity = "1";
+
+  document.getElementById("goback").style.visibility = "visible";
+  document.getElementById("goback").style.opacity = "1";
+  document.getElementById("goback").style.bottom = "-5rem";
+};
+
+document.getElementById("btnLv2").onclick = function () {
+  document.getElementById("btnLv1Anim").style.top = "200px";
+  document.getElementById("btnLv1Anim").style.visibility = "hidden";
+  document.getElementById("btnLv1Anim").style.opacity = "0";
+
+  document.getElementById("btn3Lv1Anim").style.top = "0px";
+  document.getElementById("btn3Lv1Anim").style.visibility = "visible";
+  document.getElementById("btn3Lv1Anim").style.opacity = "1";
+
+  document.getElementById("goback").style.visibility = "visible";
+  document.getElementById("goback").style.opacity = "1";
+  document.getElementById("goback").style.bottom = "20rem";
+};
+
+document.getElementById("btnLv3").onclick = function () {
+  document.getElementById("btnLv1Anim").style.top = "200px";
+  document.getElementById("btnLv1Anim").style.visibility = "hidden";
+  document.getElementById("btnLv1Anim").style.opacity = "0";
+
+  document.getElementById("btn4Lv1Anim").style.top = "0px";
+  document.getElementById("btn4Lv1Anim").style.visibility = "visible";
+  document.getElementById("btn4Lv1Anim").style.opacity = "1";
+
+  document.getElementById("goback").style.visibility = "visible";
+  document.getElementById("goback").style.opacity = "1";
+  document.getElementById("goback").style.bottom = "20rem";
+};
+
+document.getElementById("btnLv4").onclick = function () {
+  document.getElementById("btnLv1Anim").style.top = "200px";
+  document.getElementById("btnLv1Anim").style.visibility = "hidden";
+  document.getElementById("btnLv1Anim").style.opacity = "0";
+
+  document.getElementById("btn5Lv1Anim").style.top = "0px";
+  document.getElementById("btn5Lv1Anim").style.visibility = "visible";
+  document.getElementById("btn5Lv1Anim").style.opacity = "1";
+
+  document.getElementById("goback").style.visibility = "visible";
+  document.getElementById("goback").style.opacity = "1";
+  document.getElementById("goback").style.bottom = "20rem";
+};
+
+document.getElementById("btnLv5").onclick = function () {
+  document.getElementById("btnLv1Anim").style.top = "200px";
+  document.getElementById("btnLv1Anim").style.visibility = "hidden";
+  document.getElementById("btnLv1Anim").style.opacity = "0";
+
+  document.getElementById("btn6Lv1Anim").style.top = "0px";
+  document.getElementById("btn6Lv1Anim").style.visibility = "visible";
+  document.getElementById("btn6Lv1Anim").style.opacity = "1";
+
+  document.getElementById("goback").style.visibility = "visible";
+  document.getElementById("goback").style.opacity = "1";
+  document.getElementById("goback").style.bottom = "20rem";
+};
+
+document.getElementById("goback").onclick = function () {
+  document.getElementById("btn2Lv1Anim").style.top = "200px";
+  document.getElementById("btn2Lv1Anim").style.visibility = "hidden";
+  document.getElementById("btn2Lv1Anim").style.opacity = "0";
+  
+  document.getElementById("btn3Lv1Anim").style.top = "200px";
+  document.getElementById("btn3Lv1Anim").style.visibility = "hidden";
+  document.getElementById("btn3Lv1Anim").style.opacity = "0";
+  
+  document.getElementById("btn4Lv1Anim").style.top = "200px";
+  document.getElementById("btn4Lv1Anim").style.visibility = "hidden";
+  document.getElementById("btn4Lv1Anim").style.opacity = "0";
+  
+  document.getElementById("btn5Lv1Anim").style.top = "200px";
+  document.getElementById("btn5Lv1Anim").style.visibility = "hidden";
+  document.getElementById("btn5Lv1Anim").style.opacity = "0";
+  
+  document.getElementById("btn6Lv1Anim").style.top = "200px";
+  document.getElementById("btn6Lv1Anim").style.visibility = "hidden";
+  document.getElementById("btn6Lv1Anim").style.opacity = "0";
+
+  document.getElementById("btnLv1Anim").style.top = "0px";
+  document.getElementById("btnLv1Anim").style.visibility = "visible";
+  document.getElementById("btnLv1Anim").style.opacity = "1";
+
+  document.getElementById("goback").style.visibility = "hidden";
+  document.getElementById("goback").style.opacity = "0";
+  document.getElementById("goback").style.bottom = "-8rem";
+};
